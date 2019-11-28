064701c05f8f25f4ac2df60772a246f1a965f511
scgallagher <seangallagher135@gmail.com>
2019-11-27 22:08:51 -0500

    removed __init__ from PredictionView to handle analyzer creation and learning using class variables

--- backend/server/sentiment_analyzer/views.py
+++ backend/server/sentiment_analyzer/views.py
@@ -6,23 +6,14 @@ import json
 
 class PredictionView(View):
 
-    def __init__(self):
-
-        # If analyzer doesn't exist, instantiate it
-        try:
-            self.__class__.analyzer
-        except AttributeError:
-            self.__class__.analyzer = LatentSemanticAnalyzer()
-            self.__class__.analyzer.learn()
+    analyzer = LatentSemanticAnalyzer()
+    analyzer.learn()
 
     def post(self, request):
         body_unicode = request.body.decode('utf-8')
         body = json.loads(body_unicode)
         docs = body.get('documents')
 
-        if self.__class__.analyzer is None:
-            self.__class__.analyzer = LatentSemanticAnalyzer()
-            self.__class__.analyzer.learn()
         topic_predictions = self.__class__.analyzer.predict_topics(docs)
 
         return JsonResponse({"topic_predictions": topic_predictions})
